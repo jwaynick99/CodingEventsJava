c8853c324c24217dc580eb7a3f16b6bd4db18566
jwaynick99 <jwaynick99@gmail.com>
2024-10-17 18:00:20 -0500

    Ch 15 Exercises and Examples

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,7 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.*;
+
 import java.util.Objects;
 
 /**
@@ -10,14 +12,48 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
+
+    @Size(max = 500, message = "Description too long!")
     private String description;
 
-    public Event(String name, String description) {
-        this.name = name;
-        this.description = description;
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email, try again.")
+    private String contactEmail;
+
+    private EventType type;
+
+    @NotBlank(message = "Location is required")
+    private String location;
+
+    @AssertTrue(message = "Registration is required for all events.")
+    private boolean registrationRequired;
+
+    @Positive(message = "Number of attendees must be more than zero.")
+    private int numberOfAttendees;
+
+    public Event(){
         this.id = nextId;
         nextId++;
+    }
+
+    public Event(String name, String description, String contactEmail, EventType type) {
+        this();
+        this.name = name;
+        this.description = description;
+        this.contactEmail = contactEmail;
+        this.type = type;
+
+    }
+
+    public @Email String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(@Email String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public String getName() {
@@ -38,6 +74,40 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
+    }
+
+    public @NotBlank(message = "Location is required") String getLocation() {
+        return location;
+    }
+
+    public void setLocation(@NotBlank(message = "Location is required") String location) {
+        this.location = location;
+    }
+
+    @AssertTrue
+    public boolean isRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(@AssertTrue boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
+    }
+
+    @Positive(message = "Number of attendees must be more than zero.")
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(@Positive(message = "Number of attendees must be more than zero.") int numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
     }
 
     @Override
